895188fbdeb9b023974ae8736553fae7ce644462
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-27 19:01:31 -0300

    add some rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        curly: 'off',
+        'react-native/no-inline-styles': 'off',
+      },
+    },
+  ],
 };
