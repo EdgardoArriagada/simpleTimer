58db8ef4704e48a8d8afb78cde8b3d0eb8299f8a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-27 20:43:57 -0300

    add countdown to sessionscreen to manual test

--- src/presentations/screens/SessionScreen.tsx
+++ src/presentations/screens/SessionScreen.tsx
@@ -1,10 +1,13 @@
 import {FC} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {useCountdown} from '../hooks/useCountdown';
+import {formatSecondsToClock} from '../utils/time/time';
 
 export const SessionScreen: FC = () => {
+  const countdown = useCountdown(10);
   return (
     <View style={s.container}>
-      <Text>SessionScreen</Text>
+      <Text>{formatSecondsToClock(countdown)}</Text>
     </View>
   );
 };
