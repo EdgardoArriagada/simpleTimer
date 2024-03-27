eb41ea0638c3e1b61be6c5fba6f7209eaa449777
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-27 20:41:21 -0300

    refactor tests

--- src/presentations/utils/time/__tests__/time.spec.ts
+++ src/presentations/utils/time/__tests__/time.spec.ts
@@ -1,25 +1,29 @@
 import {formatSecondsToClock, formatClockToSeconds} from '../time';
 
 describe('formatSecondsToClock', () => {
-  it('formats seconds to clock', () => {
-    expect(formatSecondsToClock(0)).toBe('00:00');
-    expect(formatSecondsToClock(1)).toBe('00:01');
-    expect(formatSecondsToClock(59)).toBe('00:59');
-    expect(formatSecondsToClock(60)).toBe('01:00');
-    expect(formatSecondsToClock(61)).toBe('01:01');
-    expect(formatSecondsToClock(119)).toBe('01:59');
-    expect(formatSecondsToClock(120)).toBe('02:00');
+  it.each([
+    [0, '00:00'],
+    [1, '00:01'],
+    [59, '00:59'],
+    [60, '01:00'],
+    [61, '01:01'],
+    [119, '01:59'],
+    [120, '02:00'],
+  ])('formats seconds to clock', (seconds, clock) => {
+    expect(formatSecondsToClock(seconds)).toBe(clock);
   });
 });
 
 describe('formatClockToSeconds', () => {
-  it('formats clock to seconds', () => {
-    expect(formatClockToSeconds('00:00')).toBe(0);
-    expect(formatClockToSeconds('00:01')).toBe(1);
-    expect(formatClockToSeconds('00:59')).toBe(59);
-    expect(formatClockToSeconds('01:00')).toBe(60);
-    expect(formatClockToSeconds('01:01')).toBe(61);
-    expect(formatClockToSeconds('01:59')).toBe(119);
-    expect(formatClockToSeconds('02:00')).toBe(120);
+  it.each([
+    ['00:00', 0],
+    ['00:01', 1],
+    ['00:59', 59],
+    ['01:00', 60],
+    ['01:01', 61],
+    ['01:59', 119],
+    ['02:00', 120],
+  ])('formats clock to seconds', (clock, seconds) => {
+    expect(formatClockToSeconds(clock)).toBe(seconds);
   });
 });
