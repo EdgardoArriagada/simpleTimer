aa7ab089bf33d5101f595274fb05d8eb8063af5c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-28 20:01:30 -0300

    wip interface

--- src/presentations/screens/SessionScreen.tsx
+++ src/presentations/screens/SessionScreen.tsx
@@ -4,13 +4,37 @@ import {useCountdown} from '../hooks/useCountdown';
 import {formatSecondsToClock} from '../utils/time/time';
 import {Button} from '../components/Button';
 
+const INITIAL_SESSION_DURATION = 10;
+
+type ReadySessionProps = {
+  start: () => void;
+};
+
+const ReadySession: FC<ReadySessionProps> = ({start}) => {
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderWidth: 1,
+        padding: 10,
+      }}>
+      <Text>{formatSecondsToClock(INITIAL_SESSION_DURATION)}</Text>
+      <Button onPress={start}>▶</Button>
+    </View>
+  );
+};
+
 export const SessionScreen: FC = () => {
-  const [countdown, isRunning, start] = useCountdown(10);
+  const [countdown, isRunning, start] = useCountdown(INITIAL_SESSION_DURATION);
+  console.log('le isRunning', isRunning);
 
   return (
     <View style={s.container}>
       {isRunning && <Text>{formatSecondsToClock(countdown)}</Text>}
-      {!isRunning && <Button onPress={start}>Start</Button>}
+      {!isRunning && <ReadySession start={start} />}
     </View>
   );
 };
