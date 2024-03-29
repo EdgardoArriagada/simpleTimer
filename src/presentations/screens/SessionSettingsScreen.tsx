0f0d4d8b390e002b8ebae9fc9164eec515eabbf4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-29 19:58:41 -0300

    add a simple wheel picker

--- src/presentations/screens/SessionSettingsScreen.tsx
+++ src/presentations/screens/SessionSettingsScreen.tsx
@@ -1,10 +1,38 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+
 import {View, Text} from 'react-native';
 
+import WheelPicker from 'react-native-wheely';
+
+const makeTimeElements = (length: number) =>
+  Array.from({length}, (_, i) => i.toString().padStart(2, '0'));
+
+const allMinutes = makeTimeElements(60);
+const allHours = makeTimeElements(24);
+
 export const SessionSettingsScreen: FC = () => {
+  const [hour, setHour] = useState(() => allHours[0]);
+  const [minute, setMinute] = useState(() => allMinutes[0]);
+
   return (
-    <View>
-      <Text>Session Settings Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <WheelPicker
+        selectedIndex={allMinutes.indexOf(minute)}
+        options={allMinutes}
+        onChange={index => setMinute(allMinutes[index])}
+      />
+      <Text>:</Text>
+      <WheelPicker
+        selectedIndex={allHours.indexOf(hour)}
+        options={allHours}
+        onChange={index => setHour(allHours[index])}
+      />
     </View>
   );
 };
