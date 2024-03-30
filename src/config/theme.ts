532bea856560e6e7cfca5e3b9fb49d6ecdf32fd0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-30 19:30:07 -0300

    move styles close to components

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -10,26 +10,4 @@ export const colors = {
   background: '#000000',
 };
 
-export const gs = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-});
+export const gs = StyleSheet.create({});
