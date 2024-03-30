c27506c30422941d389217694732c652856a518e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-30 15:12:36 -0300

    configure store to receibe repeats

--- src/presentations/store/session-store.ts
+++ src/presentations/store/session-store.ts
@@ -4,15 +4,19 @@ import {formatSecondsToClock, formatClockToSeconds} from '../utils/time/time';
 
 export type SessionStore = {
   seconds: number;
+  repeats: number;
   changeSeconds: (seconds: number) => void;
   changeSecondsFromTime: (time: string) => void;
+  changeRepeats: (repeats: number) => void;
 };
 
 export const useSessionStore = create<SessionStore>()(set => ({
   seconds: 10,
+  repeats: 4,
   changeSeconds: (seconds: number) => set({seconds}),
   changeSecondsFromTime: (time: string) =>
     set({seconds: formatClockToSeconds(time)}),
+  changeRepeats: (repeats: number) => set({repeats}),
 }));
 
 export const useMemoizedClock = () => {
